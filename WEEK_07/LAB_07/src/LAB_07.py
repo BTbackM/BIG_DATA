05921c4d05a1196ab1e4330b39fb3bc52212d8da
BTbackM <felix.blanco@utec.edu.pe>
2023-05-10 23:42:49 -0500

    feat: LAB 07 files

--- WEEK_07/LAB_07/src/LAB_07.py
+++ WEEK_07/LAB_07/src/LAB_07.py
@@ -1,15 +1,16 @@
+from dask_sql import Context
 from glob import glob
 from os import path
 
-import duckdb as db
 import pandas as pd
 import time
 
+c = Context()
 ABS_PATH = path.dirname(path.abspath(__file__))
-parquet_path = path.join(ABS_PATH, '../WEEK_04/data/parquet')
-benchmark_path = path.join(ABS_PATH, '../WEEK_07/LAB_07/src/benchmark_queries')
-results_path = path.join(ABS_PATH, '../WEEK_07/LAB_07/src/expected_results')
-duckdb_times = path.join(ABS_PATH, '../WEEK_07/LAB_07/src/duckdb_times.csv')
+parquet_path = path.join(ABS_PATH, '../../../WEEK_04/data/parquet')
+benchmark_path = path.join(ABS_PATH, 'benchmark_queries')
+results_path = path.join(ABS_PATH, 'dask_results')
+dask_times = path.join(ABS_PATH, '../dask_times.csv')
 
 def read_parquet(name):
     return pd.read_parquet(path.join(parquet_path, name))
@@ -20,14 +21,15 @@ def read_benchmark(name):
 
 def make_query(query, name):
     start = time.time()
-    output = db.sql(query)
+    output = c.sql(query)
+    output.compute()
     end = time.time()
-    with open(duckdb_times, 'a') as f:
+    with open(dask_times, 'a') as f:
         f.write(f'{name},{(end - start) * 1000}\n')
     # Results to CSV
-    column_names = [desc[0] for desc in output.description]
-    df = pd.DataFrame(output.fetchall(), columns=column_names)
-    df.to_csv(path.join(results_path, f'{name:02d}.csv'), index=False)
+    # column_names = [desc[0] for desc in output.description]
+    # df = pd.DataFrame(output.fetchall(), columns=column_names)
+    # df.to_csv(path.join(results_path, f'{name:02d}.csv'), index=False)
 
 def queries():
     # Read parquet files
@@ -39,6 +41,15 @@ def queries():
     partsupp = read_parquet('partsupp.parquet')
     region = read_parquet('region.parquet')
     supplier = read_parquet('supplier.parquet')
+    # Dask temporary tables
+    c.create_table('customer', customer)
+    c.create_table('lineitem', lineitem)
+    c.create_table('nation', nation)
+    c.create_table('orders', orders)
+    c.create_table('part', part)
+    c.create_table('partsupp', partsupp)
+    c.create_table('region', region)
+    c.create_table('supplier', supplier)
 
     # Do every query
     for i in range(1, 23):
