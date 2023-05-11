44e33ac789ba1d2f9641a7adc7f458515487c6f3
BT <felix.blanco@utec.edu.pe>
2023-05-10 21:40:12 -0500

    feat: Lab 06 python files

--- WEEK_06/LAB_06.py
+++ WEEK_06/LAB_06.py
@@ -7,32 +7,37 @@ import duckdb as db
 ABS_PATH = path.dirname(path.abspath(__file__))
 parquet_path = path.join(ABS_PATH, '../WEEK_04/data/parquet')
 
+def read_parquet(name):
+    return pd.read_parquet(path.join(parquet_path, name))
+
 def queries():
     # Read parquet files
-    files = glob(path.join(parquet_path, '*.parquet'))
-    tables = {}
-    for file in files:
-        # Get table name
-        table_name = path.splitext(path.basename(file))[0]
-        # Read file
-        df = pd.read_parquet(file)
-        tables[table_name] = df
+    customer = read_parquet('customer.parquet')
+    lineitem = read_parquet('lineitem.parquet')
+    nation = read_parquet('nation.parquet')
+    orders = read_parquet('orders.parquet')
+    part = read_parquet('part.parquet')
+    partsupp = read_parquet('partsupp.parquet')
+    region = read_parquet('region.parquet')
+    supplier = read_parquet('supplier.parquet')
 
     # Query 1
-    query_1 = f"""
+    query_1 = """
     select
         sum(l_extendedprice * l_discount) as revenue
     from
-        {tables['lineitem']}
+        lineitem
     where
         l_shipdate >= date '1994-01-01'
         and l_shipdate < date '1994-01-01' + interval '1' year
         and l_discount between .06 - 0.01 and .06 + 0.01
         and l_quantity < 24;
     """
+    print(lineitem.columns)
     # output = db.sql(query_1)
     # print(output)
-    print(tables['lineitem'].head())
+    # print(tables['lineitem'].head())
+    print(query_1)
 
 def main():
     queries()
